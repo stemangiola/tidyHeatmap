6be90b7abb30ae91c83000f99d91110edf6afeb7
stemangiola <mangiolastefano@gmail.com>
2020-01-14 14:14:44 +1100

    add one level grouping again

--- R/functions.R
+++ R/functions.R
@@ -10,15 +10,19 @@
 #' @importFrom rlang quo_name
 #' @importFrom circlize colorRamp2
 #' @importFrom viridis viridis
-#' @importFrom ggplot2 unit
+#' @importFrom grid unit
+#' @importFrom grid gpar
+#' @importFrom purrr map
+#' @importFrom magrittr equals
 #'
 #' @name plot_heatmap
 #' @rdname plot_heatmap
 #'
 #' @param .data A `tbl` formatted as | <SAMPLE> | <TRANSCRIPT> | <COUNT> | <...> |
-#' @param .x The name of the column horizontally presented in the heatmap
-#' @param .y The name of the column vertically presented in the heatmap
+#' @param .horizontal The name of the column horizontally presented in the heatmap
+#' @param .vertical The name of the column vertically presented in the heatmap
 #' @param .abundance The name of the transcript/gene abundance column
+#' @param annotation Vector of quotes
 #'
 #' @details To be added.
 #'
@@ -35,22 +39,22 @@
 #' 
 #'
 #' 
-plot_heatmap = function(.data, .x, .y, .abundance){
+plot_heatmap = function(.data, .horizontal, .vertical, .abundance, annotation){
 	
 	# Make col names
-	.x = enquo(.x)
-	.y = enquo(.y)
+	.horizontal = enquo(.horizontal)
+	.vertical = enquo(.vertical)
 	.abundance = enquo(.abundance)
-	
 
 	abundance_tbl = 
 		.data %>% 
-		distinct(!!.y, !!.x, !!.abundance) %>% 
-		spread(!!.x, !!.abundance) 
+		ungroup() %>%
+		distinct(!!.vertical, !!.horizontal, !!.abundance) %>% 
+		spread(!!.horizontal, !!.abundance) 
 	
 	abundance_mat =
 		abundance_tbl %>%
-		ttBulk::as_matrix(rownames=quo_name(.y)) %>%
+		as_matrix(rownames=quo_name(.vertical)) %>%
 		t() %>%
 		apply(2, function(y) (y - mean(y)) / sd(y) ^ as.logical(sd(y))) %>%
 		t() 
@@ -58,53 +62,67 @@ plot_heatmap = function(.data, .x, .y, .abundance){
 	palette_abundance = viridis(3)[1:2] %>% c("#fefada")
 	colors = colorRamp2(c(-2, 0, 2), palette_abundance )
 	
-	# # If data grouped
-	# if()
-	# group_columns_x
-	# group_columns_y
+	# See if I have grouping and setup framework
+	if("groups" %in%  (.data %>% attributes %>% names)) {
+		x_y_annotation_cols = 
+			.data %>% 
+			ungroup() %>%
+			get_x_y_annotation_columns(!!.horizontal, !!.vertical, !!.abundance) %>%
+			map(
+				~ .x %>% intersect( .data %>% attr("groups") %>% select(-.rows) %>% colnames())
+			)
+		
+		# Row split
+		row_split = 
+			.data %>%
+			ungroup() %>%
+			distinct(!!.vertical, !!as.symbol(x_y_annotation_cols$vertical)) %>%
+			arrange(!!.vertical) %>%
+			pull(`Cell type`)
+		
+		left_annotation_args = 
+			list(
+				ct = anno_block(  #<<< IF i HAVE GROUPING THIS IS AUTOMATIC
+					gp = gpar(fill = ct_colors(	row_split %>% unique %>% sort	)),
+					labels = row_split %>% unique %>% sort,
+					labels_gp = gpar(col = "white"),
+					which = "row"
+				)
+			)
+		
+		left_annotation = do.call("rowAnnotation", as.list(left_annotation_args))
+		
+	} else {
+		row_split = NULL
+		left_annotation =	NULL
+	}
+	
+
 	
 	abundance_mat %>%
 		Heatmap(
-		column_title = quo_name(.x),
-		row_title = quo_name(.y),
-		width = unit(0.5 * 13, "cm"),
-		height = unit(0.5 * .data %>% distinct(!!.y) %>% nrow, "cm"),
+		column_title = quo_name(.horizontal),
+		row_title = quo_name(.vertical),
+		# width = unit(0.5 * 13, "cm"),
+		# height = unit(0.5 * .data %>% distinct(!!.vertical) %>% nrow, "cm"),
 		col = colors,
-		# row_split = 
-		# 	.data %>%
-		# 	ungroup()
-		# 	distinct(!!.y) %>%
-		# 	left_join(
-		# 		.data %>%
-		# 	)
-		# 	attr("groups") %>%
-		# 	
-		# distinct(symbol_ct, `Cell type`) %>% 
-		# 	arrange(symbol_ct) %>% pull(`Cell type`), 
-		cluster_row_slices = FALSE
+		row_split = row_split,
+		left_annotation =	left_annotation,
+		cluster_row_slices = F,
 		#,
 		#	clustering_distance_columns = robust_dist,
-		# left_annotation =
-		# 	rowAnnotation(
-		# 		ct = anno_block(  <<< IF i HAVE GROUPING THIS IS AUTOMATIC
-		# 			gp = gpar(fill = ct_colors(
-		# 				tbl %>% distinct(`Cell type`) %>% arrange(`Cell type`) %>% pull(`Cell type`) %>% as.character
-		# 			)), 
-		# 			labels = ct_names(
-		# 				tbl %>% distinct(`Cell type`) %>% arrange(`Cell type`) %>% pull(`Cell type`) %>% as.character
-		# 			), 
-		# 			labels_gp = gpar(col = "white")
-		# 		),
-		# 		
-		# 		inflection =  anno_points( << THIS CAN ALSO BE AUTOMATIC GIVING COLUMN DISTINCT WITH .Y AND TYPE anno_POINTS
-		# 			tbl %>% distinct(symbol_ct, inflection) %>% 
-		# 				arrange(symbol_ct) %>% pull(inflection)
-		# 		)
-		# 		
-		# 	),
+				# ,
+				# 
+				# inflection =  anno_points( << THIS CAN ALSO BE AUTOMATIC GIVING COLUMN DISTINCT WITH .vertical AND TYPE anno_POINTS
+				# 	tbl %>% distinct(symbol_ct, inflection) %>%
+				# 		arrange(symbol_ct) %>% pull(inflection)
+				# )
+
+
 		# top_annotation  =
-		# 	HeatmapAnnotation(  <<< IF i HAVE GROUPING THIS IS AUTOMATIC
-		# 		`CAPRA-S` =  tbl %>% distinct(UBR, CAPRA_TOTAL) %>% 
+		# 	HeatmapAnnotation(  # <<< IF i HAVE GROUPING THIS IS AUTOMATIC
+		# 		df = 
+		# 		`CAPRA-S` =  .data %>% distinct(UBR, CAPRA_TOTAL) %>%
 		# 			arrange(UBR) %>% pull(CAPRA_TOTAL) ,
 		# 		col = list(	`CAPRA-S`  = circlize::colorRamp2(0:7, colorRampPalette(RColorBrewer ::brewer.pal(11,"Spectral") %>% rev)(8)))
 		# 	)
--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,17 +1,35 @@
 context('tests')
 
 
-test_that("...",{
+test_that("basic plot",{
 
 	p = 
 		tidyHeatmap::plot_heatmap(
-			filter(tidyHeatmap::N52, Category == "Angiogenesis"),
-			.x = UBR, 
-			.y = symbol_ct, 
+			dplyr::filter(tidyHeatmap::N52, Category == "Angiogenesis"),
+			.horizontal = UBR, 
+			.vertical = symbol_ct, 
 			.abundance = `read count normalised log`
 		)
 	
 	
   expect_equal(as.character(class(p)), "Heatmap" )
 
+})
+
+test_that("grouped plot",{
+	
+	p = 
+		tidyHeatmap::plot_heatmap(
+			dplyr::group_by(
+				dplyr::filter(tidyHeatmap::N52, Category == "Angiogenesis"),
+				`Cell type`
+				),
+			.horizontal = UBR, 
+			.vertical = symbol_ct, 
+			.abundance = `read count normalised log`
+		)
+	
+	
+	expect_equal(as.character(class(p)), "Heatmap" )
+	
 })
